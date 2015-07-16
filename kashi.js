a460af0b8c704ea9f7161dc37fd8c0f0fb1c1706
N.Shimizu <chikoski@gmail.com>
2015-07-16 10:18:21 +0900

    initial commit

---
+++ kashi.js
@@ -0,0 +1,27 @@
+function kashi1(){
+set.Timeout('kashi1()', 13000) //13
+var txt = "降り始めた雪は　足跡消して"
+ctx1.fillStyle = "#00bfff";
+ctx1.font = 'bold 50px "HG丸ゴシックM-PRO"';
+}
+
+function kashi2(){
+set.Timeout('kashi2()', 6000) //19
+var txt = "真っ白な世界に　ひとりのわたし"
+ctx1.fillStyle = "#00bfff";
+ctx1.font = 'bold 50px "HG丸ゴシックM-PRO"';
+}
+
+function kashi3(){
+set.Timeout('kashi3()', 6000) //27
+var txt = "風が心にささやくの　このままじゃ　ダメなんだと"
+ctx1.fillStyle = "#00bfff";
+ctx1.font = 'bold 50px "HG丸ゴシックM-PRO"';
+}
+
+function kashi4(){
+set.Timeout('kashi()', 13000)
+var txt = "降り始めた雪は　足跡消して"
+ctx1.fillStyle = "#00bfff";
+ctx1.font = 'bold 50px "HG丸ゴシックM-PRO"';
+}
